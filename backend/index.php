1b695e501cf4c311c3da31942f4f719c79a06724
TheRealColeG <98725167+TheRealColeG@users.noreply.github.com>
2023-02-24 11:53:41 -0500

    Created backend folder and moved index.php into it
    This website isn't really used, more of a basis for PHP understanding - this SHOULD be what the front-end has, in some sense.

---
+++ backend/index.php
@@ -0,0 +1,25 @@
+<html>
+<title>Hi</title>
+<h1>Please Log In</h1>
+<body>
+<?php echo "Welcome!" ?>
+  
+        <h1>SQL Test</h1>
+        <form action="sqltest.php" method="post">
+        <br>Username: <input type="text" name="username">
+        <br>Email: <input type="text" name="email">
+        <br>Password: <input type="password" name="password">
+        <br><input type="submit" name="submit">
+        </form>
+
+        <br><br>
+        <h1>PHP Test</h1>
+        <form action="sqltest.php" method="post">
+        <br>Username: <input type="text" name="username">
+        <br>Email: <input type="text" name="email">
+        <br>Password: <input type="password" name="password">
+        <br><input type="submit" name="submit">
+        </form>
+
+</body>
+</html>
