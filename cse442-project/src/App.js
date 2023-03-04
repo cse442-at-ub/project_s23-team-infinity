1b8f288742d59d33646df540203850db13ea4b11
Crend <a2253380891@gmail.com>
2023-03-04 14:08:25 -0500

    Connecting login page to the forgot password page task#26

--- cse442-project/src/App.js
+++ cse442-project/src/App.js
@@ -1,50 +1,22 @@
-import { useState } from "react";
-import $ from "jquery";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import Loginin from './Login'
+import Recovery from './Recovery'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 
 
-function App() {
-  const [name, setName] = useState("");
-    const [result, setResult] = useState("");
-
-    const handleChange = (e) => {
-        setName(e.target.value);
-    };
-
-    const handleSumbit = (e) => {
-        e.preventDefault();
-        const form = $(e.target);
-        $.ajax({
-            type: "POST",
-            url: form.attr("action"),
-            data: form.serialize(),
-            success(data) {
-                setResult(data);
-            },
-        });
-    };
-
+function Login() {
     return (
-        <div className="App">
-            <form
-                action="http://localhost:8000/server.php"
-                method="post"
-                onSubmit={(event) => handleSumbit(event)}
-            >
-                <label htmlFor="name">Name: </label>
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={name}
-                    onChange={(event) => handleChange(event)}
-                />
-                <br />
-                <button type="submit">Submit</button>
-            </form>
-            <h1>{result}</h1>
-        </div>
-    );
+	<Router>
+	<div className="login-page">
+    <Routes>
+      <Route path="/" element={<Loginin/>} />
+      <Route path="/recovery" element={<Recovery/>} />
+    </Routes>
+  </div>
+    </Router>
+  );
 }
 
-export default App;
+export default Login;
---
+++ cse442-project/src/Recovery.js
@@ -0,0 +1,9 @@
+const Recovery=()=>{
+    return(
+        <div>
+              Welcome to Recovery Page
+        </div>
+    )
+}
+
+export default Recovery;
