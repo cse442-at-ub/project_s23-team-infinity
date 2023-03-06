5b6eaa50b692622e3676e28b0e6ff9ababd40abe
Danielle Akosua <danielleakosua@gmail.com>
2023-03-06 02:23:18 -0500

    Set cookies to be able to remember user #9

--- hello-world-project/src/SetCookies.js
+++ hello-world-project/src/SetCookies.js
@@ -1,6 +1,5 @@
 import *as React from "react";
 import { useState } from 'react';
-import {BrowserRouter as Router, Routes,Route,Link,useNavigate } from 'react-router-dom';
 
 function setCookie(){
     var u = document.getElementById('username').value
@@ -8,4 +7,5 @@ function setCookie(){
 
     document.cookie = "myusername"+u+";path=http://localhost:3000/"
     document.cookie = "myusername"+p+";path=http://localhost:3000/"
-}
+};
+export default setCookie
