2ff4b8fccdb6ecfee3ee863d18bea1bf13b4b0e2
Danielle Akosua <danielleakosua@gmail.com>
2023-03-05 22:04:13 -0500

    Ensuring user info is remembered after checking Remember Me box #9

--- hello-world-project/src/App.js
+++ hello-world-project/src/App.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import $ from "jquery";
 import "./App.css";
+import Loginin from './Login'
+import SetCookies from './SetCookies'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+
 
 function Remember () {
   return (
     <Router>
       <div className="remember-me">
         <Routes>
+          <Route path="/" element={<Loginin/>} />
           <Route path="/setcookies" element={<SetCookies/>} />
         </Routes>
       </div>
