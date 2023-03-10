66139ed2cc35ae2cb7e318fb693686a02bc36890
Danielle Akosua <danielleakosua@gmail.com>
2023-03-10 01:45:02 -0500

    Check contents of the cookie #9

--- hello-world-project/src/CheckCookie.js
+++ hello-world-project/src/CheckCookie.js
@@ -1,14 +1,22 @@
 import getCookie from "./GetCookieData";
+import setCookie from "./SetCookies";
+
+// function checkCookie() {
+//     var user=getCookie("username");//need
+//     if (user != "") {//dont need this conditional
+//         alert("Welcome again " + user);//dont need
+//     } else {
+//         user = prompt("Please enter your email:","");//dont need this prompt
+//         if (user != "" && user != null) {
+//             setCookie("username", user, 30);
+//         }
+//     }
+// }
 
 function checkCookie() {
-    var user=getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
-    } else {
-        user = prompt("Please enter your name:","");
-        if (user != "" && user != null) {
-            setCookie("username", user, 30);
-        }
-    }
+    var user=getCookie("username;SameSite=None");//need
+        //if (user != "" && user != null) {
+    setCookie("email", user, 30);//first arg sets name
+      //  }
 }
 export default checkCookie
