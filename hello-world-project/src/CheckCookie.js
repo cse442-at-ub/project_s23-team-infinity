8a2ec5cb80e920682751c47e0c659e88da58cc12
Danielle Akosua <danielleakosua@gmail.com>
2023-03-13 16:49:16 -0400

    Remember me functionality #9

--- hello-world-project/src/CheckCookie.js
+++ hello-world-project/src/CheckCookie.js
@@ -1,36 +1,36 @@
 import getCookie from "./GetCookieData";
 import setCookie from "./SetCookies";
 
-// function checkCookie() {
-//     var user=getCookie("username");//need
-//     if (user != "") {//dont need this conditional
-//         alert("Welcome again " + user);//dont need
-//     } else {
-//         user = prompt("Please enter your email:","");//dont need this prompt
-//         if (user != "" && user != null) {
-//             setCookie("username", user, 30);
-//         }
-//     }
-// }
-
-function checkCookie() {//mess around with combining this and the other 2 functions
+function checkCookie() {
     var user=getCookie("username");//need
-        //if (user != "" && user != null) {
-    setCookie("email", user, 30);//first arg sets name
-      //  }
-            // function checkCookie() {
-            // var user=getCookie("username");
-            // var uname=document.login.uname.value;
-            // if (user == uname) {
-            //     alert("Welcome again " + user);
-            //     var myWindow = window.open("", "_self");
-            //     myWindow.document.write("<p>Welcome to LTI</p>");
-            // } else {
-            //     if (user != "" && user != null) {
-            //     setCookie();
-            //     validation();
-            //     }
-            // }
-            
+    if (user != "") {//dont need this conditional
+        alert("Welcome again " + user);//dont need
+    } else {
+        user = prompt("Please enter your email:","");//dont need this prompt
+        if (user != "" && user != null) {
+            setCookie("username", user, 30);
+        }
+    }
 }
+
+// function checkCookie() {//mess around with combining this and the other 2 functions
+//     var user=getCookie("username");//need
+//         //if (user != "" && user != null) {
+//     setCookie("email", user, 30);//first arg sets name
+//       //  }
+//             // function checkCookie() {
+//             // var user=getCookie("username");
+//             // var uname=document.login.uname.value;
+//             // if (user == uname) {
+//             //     alert("Welcome again " + user);
+//             //     var myWindow = window.open("", "_self");
+//             //     myWindow.document.write("<p>Welcome to LTI</p>");
+//             // } else {
+//             //     if (user != "" && user != null) {
+//             //     setCookie();
+//             //     validation();
+//             //     }
+//             // }
+            
+// }
 export default checkCookie
