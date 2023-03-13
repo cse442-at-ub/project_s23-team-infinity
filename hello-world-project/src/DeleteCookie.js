9056e1f913a5fdc0ca80c5bbbfc81e83bce82739
Danielle Akosua <danielleakosua@gmail.com>
2023-03-13 16:50:17 -0400

    Remember me functionality for deleting cookie #9

--- hello-world-project/src/DeleteCookie.js
+++ hello-world-project/src/DeleteCookie.js
@@ -2,7 +2,7 @@
 function deleteCookie(cname)
 {
 	//$.removeCookie('UNFCUCuser', { path: '/' });
-	document.cookie = cname + '=; expires=Thu, 01-Jan-70 00:00:01 GMT;path=/';
+	document.cookie = cname + '=; expires=Thu, 01-Jan-70 00:00:01 GMT;path=/';//need to call delete cookie for when user logsout
 		
 }
 export default deleteCookie
