fe26ab838e4ee2d62f5ee16b4a079306b111cb58
TheRealColeG <98725167+TheRealColeG@users.noreply.github.com>
2023-04-20 19:59:14 -0400

    Hopefully valid php files for both update and delete events here - #63 and #64

--- cse442-project/PHP/deleteevent.php
+++ cse442-project/PHP/deleteevent.php
@@ -2,40 +2,56 @@
 
     if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
 
-        echo "<br>Delete event request received<br>";
-        $username = $_POST["username"];
         $eventId = $_POST["eventId"];
         $token = $_POST["token"];
 
-        if (strlen(trim($username)) == 0 or 
-            strlen(trim($eventId)) == 0 or 
+        if (strlen(trim($eventId)) == 0 or 
             strlen(trim($token)) == 0) {
-                echo "<br>New event requires both username and event title";
+                echo "New event requires both username and event title";
                 die();
         }
 
         else {
             $dbhost = 'oceanus';
             $dbname = 'cse442_2023_spring_team_ad_db';
-            $tablename = 'Users';
             $dbuser = 'colegrab';
             $dbpass = '50225546';
 
             $connection = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die("Connection Failed: " . mysqli_connect_error());
         
-            $stmt = mysqli_prepare($connection, "SELECT UserID FROM Users WHERE Username=? AND Usertoken=?");
-            //Following copied from the PHP mysqli Documentation for prepared statements: https://www.php.net/manual/en/mysqli.prepare.php
-            $first = mysqli_stmt_bind_param($stmt, "ss", $username, $token);
+            //find userID associated with token
+            $stmt = mysqli_prepare($connection, "SELECT UserID FROM Users WHERE Usertoken=?");
+            $first = mysqli_stmt_bind_param($stmt, "s", $token);
             $second = mysqli_stmt_execute($stmt);
             $third = mysqli_stmt_bind_result($stmt, $userID);
-            $fourth = mysqli_stmt_fetch($stmt);
+            $fetch = mysqli_stmt_fetch($stmt);
+            
+            //if user found with that ID
+            if ($fetch) {
+                //Connect to EVENTS table for two reasons: A. Verify that userID+eventID match, and to fill in empty spots
+                $eventConn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die("Connection Failed: " . mysqli_connect_error());
+                $eventstmt = mysqli_prepare($connection, "SELECT Title, Date, Time, Location, Notes FROM Events WHERE UserID = ? AND EventID = ?");
+                $first = mysqli_stmt_bind_param($eventstmt, "ss", $userID, $eventId);
+                $second = mysqli_stmt_execute($eventstmt);
+                $third = mysqli_stmt_bind_result($eventstmt, $title_stored, $date_stored, $time_stored, $location_stored, $notes_stored);
+                $found = mysqli_stmt_fetch($eventstmt);
 
-            //if the username exists AND token is correct
-            if ($userID > 0) {
-                $prepstmt = mysqli_prepare($newConn, "DELETE FROM Events WHERE eventId = ?");
-                $first = mysqli_stmt_bind_param($prepstmt, "s", $eventId);
-                $second = mysqli_stmt_execute($prepstmt);
-                echo "<br>executed";
+                //There is a matching userID and eventID pair - therefore can be removed
+                if ($found) {
+                    $deleteConn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die("Connection Failed: " . mysqli_connect_error());
+                    $deletestmt = mysqli_prepare($deleteConn, "DELETE FROM Events WHERE EventId = ? AND UserID = ?");
+                    $first = mysqli_stmt_bind_param($eventstmt, "ss", $userID, $eventId);
+                    $done = mysqli_stmt_execute($eventstmt);
+
+                    if ($done) {
+                        echo "Event Removed!";
+                        die();
+                    }
+                    else {
+                        echo "Error when trying to remove event";
+                        die();
+                    }
+                }
             }
         }
     }
--- cse442-project/PHP/updateevent.php
+++ cse442-project/PHP/updateevent.php
@@ -12,34 +12,56 @@
         $notes = $_POST['notes'];
 
         if (strlen(trim($eventId)) == 0 or strlen(trim($token)) == 0) {
-                echo "<br>New event requires both username and event title";
+                echo "New event requires both username and event title";
                 die();
         }
 
         else {
             $dbhost = 'oceanus';
             $dbname = 'cse442_2023_spring_team_ad_db';
-            $tablename = 'Users';
             $dbuser = 'colegrab';
             $dbpass = '50225546';
 
+            //Connect to USER table to get UserID from Token
             $connection = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die("Connection Failed: " . mysqli_connect_error());
-        
-            $stmt = mysqli_prepare($connection, "SELECT UserID FROM Users WHERE Username=? AND Usertoken=?");
-            //Following copied from the PHP mysqli Documentation for prepared statements: https://www.php.net/manual/en/mysqli.prepare.php
-            $first = mysqli_stmt_bind_param($stmt, "ss", $username, $token);
+            $stmt = mysqli_prepare($connection, "SELECT UserID FROM Users WHERE token=?");
+            $first = mysqli_stmt_bind_param($stmt, "s", $token);
             $second = mysqli_stmt_execute($stmt);
             $third = mysqli_stmt_bind_result($stmt, $userID);
-            $fourth = mysqli_stmt_fetch($stmt);
+            $fetched = mysqli_stmt_fetch($stmt);
 
-            //if the username exists AND token is correct
-            if ($userID > 0) {
+            if ($fetched) {
+                //Connect to EVENTS table for two reasons: A. Verify that userID+eventID match, and to fill in empty spots
+                $eventConn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die("Connection Failed: " . mysqli_connect_error());
+                $eventstmt = mysqli_prepare($connection, "SELECT Title, Date, Time, Location, Notes FROM Events WHERE UserID = ? AND EventID = ?");
+                $first = mysqli_stmt_bind_param($eventstmt, "ss", $userID, $eventId);
+                $second = mysqli_stmt_execute($eventstmt);
+                $third = mysqli_stmt_bind_result($eventstmt, $title_stored, $date_stored, $time_stored, $location_stored, $notes_stored);
+                $found = mysqli_stmt_fetch($eventstmt);
 
-                $prepstmt = mysqli_prepare($newConn, "UPDATE Events SET Title = ?, Date = ?, Time = ?, Location = ?, Notes = ? WHERE eventId = ?");
-                $first = mysqli_stmt_bind_param($prepstmt, "ssssss", $title, $date, $time, $location, $notes, $eventId);
-                $second = mysqli_stmt_execute($prepstmt);
-                echo "<br>executed";
-
+                //There is a matching userID and eventID pair - therefore can be updated
+                if ($found) {
+                    $updateConn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die("Connection Failed: " . mysqli_connect_error());
+                    $prepstmt = mysqli_prepare($updateConn, "UPDATE Events SET Title = ?, Date = ?, Time = ?, Location = ?, Notes = ? WHERE eventId = ?");
+                    //if the field isn't set from front-end i.e. user didn't change it
+                    if (!isset($title)) {
+                        $title = $title_stored;
+                    }
+                    if (!isset($date)) {
+                        $date = $date_stored;
+                    }
+                    if (!isset($time)) {
+                        $time = $time_stored;
+                    }
+                    if (!isset($location)) {
+                        $location = $location_stored;
+                    }
+                    if (!isset($notes)) {
+                        $notes = $notes_stored;
+                    }
+                    $first = mysqli_stmt_bind_param($prepstmt, "ssssss", $title, $date, $time, $location, $notes, $eventId);
+                    $second = mysqli_stmt_execute($prepstmt);
+                }
             }
         }
     }
