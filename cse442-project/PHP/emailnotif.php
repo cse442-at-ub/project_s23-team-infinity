47559606dc0bd0109d212fdbd014b5a9c730dabc
duncenzh <duncenzh@buffalo.edu>
2023-04-30 22:52:58 -0400

    #73 forgot to save

---
+++ cse442-project/PHP/emailnotif.php
@@ -0,0 +1,21 @@
+<?php
+include 'database.php';
+
+$notification = $_POST[''];
+
+if ($notification) {
+    $stmt = $conn->prepare("SELECT * FROM Users WHERE token = ?");
+    $stmt->bind_param("s", $notification);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $to = $row['Email'];
+        $subject = "no-reply Event Notification";
+        $message = "You have an event coming up in 1 hour";
+        mail($to, $subject, $message);
+    }
+    $stmt->close();
+}
+?>
